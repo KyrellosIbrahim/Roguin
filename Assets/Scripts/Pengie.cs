e6b2f2e29c719466648cf51e5d0932a2c4a98ec1
Christopher Garcia <Christopherg1818@gmail.com>
2026-04-09 15:13:36 -0500

    Sprite movement (penguin)

--- Assets/Scripts/Pengie.cs
+++ Assets/Scripts/Pengie.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class Pengie : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveTime = 0.2f;
+    private bool isMoving = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isMoving) return;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            StartCoroutine(Move(Vector3.up));
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            StartCoroutine(Move(Vector3.down));
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            StartCoroutine(Move(Vector3.left));
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            StartCoroutine(Move(Vector3.right));
     }
 
+    System.Collections.IEnumerator Move(Vector3 direction)
+    {
+        isMoving = true;
+
+        Vector3 start = transform.position;
+        Vector3 end = start + direction;
+
+        float elapsed = 0;
+
+        while (elapsed < moveTime)
+        {
+            transform.position = Vector3.Lerp(start, end, elapsed / moveTime);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = end;
+        isMoving = false;
+    }
 }
