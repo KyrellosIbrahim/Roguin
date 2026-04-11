846a28ec42ce06b154c9800e03aab5be86cdba8d
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2026-04-10 20:19:35 -0500

    generate new map upon reaching exit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     public GameObject player;
+    public BoardManager boardManager;
     public string gameOverSceneName = "GameOver";
 
     private void Awake()
@@ -25,6 +26,24 @@ public class GameManager : MonoBehaviour
 
         Time.timeScale = 1f;
         SceneManager.LoadScene(gameOverSceneName);
+    }
+
+    public void NextLevel()
+    {
+        // Regenerate the board
+        if (boardManager != null)
+        {
+            boardManager.GenerateTileMap();
+            boardManager.PlaceExitTile();
+        }
+
+        // Reset player position and health
+        if (player != null)
+        {
+            Pengie pengie = player.GetComponent<Pengie>();
+            if (pengie != null)
+                pengie.ResetPlayer();
+        }
     }
 
     public void LoadNextLevel()
